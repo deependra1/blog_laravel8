4ff82892e2debeb868afe6e36ef7a5af1693ba5d
deependra1 <rythm.dipen@gmail.com>
2021-04-28 12:36:50 +0545

    side nav editing

--- resources/views/backend/incl/sidenav.blade.php
+++ resources/views/backend/incl/sidenav.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="dist/img/AdminLTELogo.png" alt="" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
@@ -109,6 +109,30 @@
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('posts.create') }}" class="nav-link {{ Route::currentRouteName()==='posts.create'?'active':'' }}">
+                                <i class="fas fa-plus nav-icon"></i>
+                                <p>Add New</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="nav-item {{ Route::currentRouteName()==='uploads.index' || Route::currentRouteName()==='uploads.create' ?'menu-open':'' }}">
+                    <a href="#" class="nav-link {{ Route::currentRouteName()==='uploads.index' || Route::currentRouteName()==='uploads.create' ?'active':'' }}" >
+                        <i class="nav-icon fas fa-folder"></i>
+                        <p>
+                            Images
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('uploads.index') }}" class="nav-link {{ Route::currentRouteName()==='uploads.index'?'active':'' }}">
+                                <i class="fas fa-list nav-icon"></i>
+                                <p>List All</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('uploads.create') }}" class="nav-link {{ Route::currentRouteName()==='uploads.create'?'active':'' }}">
                                 <i class="fas fa-plus nav-icon"></i>
                                 <p>Add New</p>
                             </a>
